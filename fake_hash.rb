508110dc3283cde634c2a2854b996b130c0011fd
Blake Johnson <blake41@gmail.com>
2014-06-02 18:14:13 -0400

    solution

--- fake_hash.rb
+++ fake_hash.rb
@@ -1,6 +1,22 @@
 class FakeHashWrapper
+  attr_accessor :elements
+  def initialize
+    @elements = {}
+  end
 
+  def []=(key, value)
+    elements[key.to_sym] = value
+  end
 
+  def [](key)
+    elements[key.to_sym]
+  end
 
+  def keys
+    elements.keys.sort.reverse
+  end
 
+  def first_letter(letter)
+    elements.values.select {|value| (elements.key(value))[0] == letter}
+  end
 end
