eb216ec7ed058aa586e7b1d3a44f49781bac9251
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-10 13:26:35 -0400

    Makes a fake hash

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,34 @@
+require 'pry'
+
 class FakeHashWrapper
+  include Enumerable
+  attr_accessor :my_hash
 
-  # write code here
+  def initialize(hash={})
+    @my_hash = hash
+  end
 
+  def [](key)
+    my_hash[key.to_sym]
+  end
+
+  def []=(key, value)
+    @my_hash[key.to_sym] = value
+  end
+
+  def keys
+    @my_hash.keys.sort.reverse
+  end
+
+  def first_letter(letter)
+    array =[]
+    @my_hash.each  do |key, value|
+      array << value if key.to_s.start_with?(letter)
+    end
+    array
+  end
+
+  def each(&block)
+    @my_hash.each(&block)
+  end
 end
